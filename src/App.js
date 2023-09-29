36754e3a23ec8f64420dbe2bcd8b00cd9d45858c
ghassant29 <gass304@gmail.com>
2023-09-29 14:19:42 +0300

    Fetching questions fake API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./main";
+import Loader from "./Loader";
+import Error from "./Error";
+const initialState = { questions: [], status: "loading" };
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, question: action.payload, status: "ready" };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action Unknown");
+  }
+}
 export default function App() {
+  const [{ question, status }, dispatch] = useReducer(initialState, reducer);
+
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+      </Main>
     </div>
   );
 }
